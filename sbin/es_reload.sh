5ce8f34c320b0a218c6c73bbadebd495396bcee6
JP Navarro <jpfnavarro@gmail.com>
2020-08-19 20:05:37 +0000

    Add es_reload run shell scripts

---
+++ sbin/es_reload.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+### RePublish Tool
+MY_BASE=/soft/warehouse-apps-1.0/tools
+
+PYTHON=python3
+PYTHON_BASE=/soft/python/python-3.7.7-base
+PYTHON_ROOT=/soft/warehouse-apps-1.0/tools/python
+source ${PYTHON_ROOT}/bin/activate
+
+export PYTHONPATH=$DAEMON_DIR/lib:/soft/warehouse-1.0/PROD/django_xsede_warehouse
+export DJANGO_CONF=/soft/warehouse-apps-1.0/tools/conf/django_xsede_warehouse.conf
+export DJANGO_SETTINGS_MODULE=xsede_warehouse.settings
+
+$MY_BASE/PROD/bin/es_reload.py -c $MY_BASE/conf/es_reload.conf $@
+RETVAL=$?
+echo rc=$RETVAL
+exit $RETVAL
